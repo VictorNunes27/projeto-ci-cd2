90d779767cf772602d1102c02626f54455c389d4
Victor Nunes <147217773+VictorNunes27@users.noreply.github.com>
2025-04-14 20:42:21 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,21 +1,5 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-
-import unittest
 from math_utils import add, subtract
-
-class TestMathUtils(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(5, 5), 10)
-        self.assertEqual(add(-1, 1), 0)
-
-    def test_subtract(self):
-        self.assertEqual(subtract(10, 6), 6)
-        self.assertEqual(subtract(0, 0), 0)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add():
+assert add(2, 3) == 5
+def test_subtract():
+assert subtract(5, 2) == 3
