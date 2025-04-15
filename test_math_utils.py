23331bb0dfdc03c26ac052aba3d93a79e67ef22f
Victor Nunes <147217773+VictorNunes27@users.noreply.github.com>
2025-04-14 21:02:33 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,17 +1,21 @@
 import pytest
-from math_utils import add, subtract
+from math_utils import add, subtract,multiply
 
 def test_add():
-    assert add(2, 3) == 5
+ assert add(2, 3) == 5
+ assert add(5, 5) == 10
 
 def test_subtract():
-    assert subtract(5, 2) == 3
+ assert subtract(5, 2) == 3
+ assert subtract(1, 1) == 0
 
-def test_multiply():
-    assert multiply(4, 3) == 12
+def teste_multiply():
+  assert multiply(4,3) == 12
+  assert multiply(2,3) == 6
 
 def test_multiply_invalid_type():
-    try:
-        multiply("a", 3)
-    except ValueError:
-        assert True
+
+ try:
+   multiply("a", 3)
+ except ValueError:
+   assert True
